b0fc15687a18fbe9258e6b86d406bf83d64f9498
Adriano Dantas <adantasgerente@gmail.com>
2026-04-13 10:54:37 -0400

    Update import google.py

--- src/import google.py
+++ src/import google.py
@@ -1,6 +1,6 @@
 import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyCJsvQSlEqLwqrLgjBwouzg55XoS7XglXo")
+genai.configure(api_key="SUA_CHAVE_AQUI")
 
 for m in genai.list_models():
     print(m.name)
